6ac8944ed556dae6a3b95022a5edc66dbe672696
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-09-04 22:57:34 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
 # Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
+# Author: Sagar Hedaoo
 # Additional Authors:
-# <your GitHub username>
+# sagarhedaoo
 
 # Input:
 # p, principal amount
